70158eea7de2f782b23fb83a59290bc2eacd8391
Dhiraj Kumar <dhiraj5_kumar@yahoo.co.in>
2026-07-14 21:41:35 +0530

    workflow added

--- src/main/java/com/example/gitaction/controller/TestController.java
+++ src/main/java/com/example/gitaction/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.example.gitaction.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TestController {
+    @GetMapping("/message")
+    public String message() {
+        return "welcome the first spring boot github publish example !!!";
+    }
 }
